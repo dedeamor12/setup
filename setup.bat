e587bc1bcbf89133f869d6b48525ab291e9f7d24
dedeamor12 <andreleandromatos@gmail.com>
2024-04-11 11:34:46 -0300

    setup feito e testado

---
+++ setup.bat
@@ -0,0 +1,9 @@
+git config --global user.name dedeamor12
+git config --global user.email andreleandromatos@gmail.com
+
+cd C:\Users\MAUA-MERCEDESVALENTI\
+
+git clone https://github.com/dedeamor12/Caesar-Cipher.git
+ cd Caesar-Cipher
+
+ code .
